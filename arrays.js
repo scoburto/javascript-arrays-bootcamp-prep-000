71223ca830a45bf3f23204e56ff1f8fa32382040
scoburto@gmail.com <scoburto@gmail.com>
2018-03-04 17:17:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,4 +33,10 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,array.length-1)
+}
