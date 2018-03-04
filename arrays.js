c3ff672da34b599ae89dc607b3f8688b770963d5
scoburto@gmail.com <scoburto@gmail.com>
2018-03-04 16:37:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers","hundred grand", "kitkat","skittles"];
 function addElementToBeginningOfArray(list,extra) {
-  var newList = [extra...list];
+  var newList = [extra,...list];
   return newList;
 }
 
